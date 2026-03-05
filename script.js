d129d0de4e6ab5444de7d839bc6c76c7ebe5954b
Tharindu Senanayake <tharindusenanayake134@gmail.com>
2026-03-05 16:10:41 +0530

    advanced portfolio

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
+function filterProjects(category){
+
+let projects=document.querySelectorAll(".card")
+
+projects.forEach(card=>{
+
+if(category==="all"){
+
+card.style.display="block"
+
+}
+
+else{
+
+if(card.classList.contains(category)){
+
+card.style.display="block"
+
+}
+
+else{
+
+card.style.display="none"
+
+}
+
+}
+
+})
+
+}
+
+
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+
 anchor.addEventListener("click",function(e){
 
 e.preventDefault()
 
 document.querySelector(this.getAttribute("href")).scrollIntoView({
+
 behavior:"smooth"
+
 })
 
 })
+
 })
